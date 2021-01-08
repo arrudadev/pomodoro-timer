0e12777b5750279eed58a533108e5c926757c736
Alexandre <alexandre.monteiro.bec@gmail.com>
2021-01-08 14:30:44 -0300

    chore: setup chakra ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 
 import { AppProps } from 'next/app';
 
+import { ChakraProvider } from '@chakra-ui/react';
+
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 };
 
 export default App;
