f0bc6ed0124a029fcc48ab97d51e800d90bc038e
Alexandre <alexandre.monteiro.bec@gmail.com>
2021-01-13 01:20:04 -0300

    chore: add Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { FaGithub } from 'react-icons/fa';
+
+import { Flex, Heading, Icon, Link } from '@chakra-ui/react';
+
+const Header: React.FC = () => {
+  return (
+    <Flex
+      as="header"
+      alignItems="center"
+      justifyContent="space-between"
+      width="100%"
+      textAlign="left"
+      padding="8px"
+    >
+      <Heading as="h2" color="#e3e4e5">
+        Pomodoro Timer
+      </Heading>
+
+      <Link
+        href="https://github.com/monteiro-alexandre/pomodoro-timer"
+        isExternal
+      >
+        <Icon
+          as={FaGithub}
+          width="30px"
+          height="30px"
+          cursor="pointer"
+          _hover={{ color: '#0098f7' }}
+          title="Pomodoro Timer GitHub"
+        />
+      </Link>
+    </Flex>
+  );
+};
+
+export default Header;
--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { Flex } from '@chakra-ui/react';
 
+import Header from '../Header';
 import Main from '../Main';
 import MenuBar from '../MenuBar';
 
@@ -13,7 +14,10 @@ const Layout: React.FC<LayoutProps> = ({ children, activePage }) => {
   return (
     <Flex height="100%" width="100%">
       <MenuBar activePage={activePage} />
-      <Main>{children}</Main>
+      <Main>
+        <Header />
+        {children}
+      </Main>
     </Flex>
   );
 };
